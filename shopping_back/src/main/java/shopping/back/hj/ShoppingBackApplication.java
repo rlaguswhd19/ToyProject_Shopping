529c7dd116ad6be188ece54f7626e5a24d2e387c
rlaguswhd19 <rlaguswhd19@naver.com>
2020-06-11 16:13:20 +0900

    shopping_back init

---
+++ shopping_back/src/main/java/shopping/back/hj/ShoppingBackApplication.java
@@ -0,0 +1,12 @@
+package shopping.back.hj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingBackApplication.class, args);
+	}
+}
