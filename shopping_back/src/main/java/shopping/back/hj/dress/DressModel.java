1c2c4004c74d958cc36844f5b08dcd10ac3853ed
rlaguswhd19 <rlaguswhd19@naver.com>
2020-06-24 12:33:06 +0900

    hateoas Dress 적용 DressModel 생성 DressModel addLink self, update, lists

---
+++ shopping_back/src/main/java/shopping/back/hj/dress/DressModel.java
@@ -0,0 +1,27 @@
+package shopping.back.hj.dress;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+
+public class DressModel extends EntityModel<Dress> {
+
+	@SuppressWarnings("deprecation")
+	public DressModel(Dress content, Link... links) {
+		super(content, links);
+		add(linkTo(DressController.class).slash(content.getId()).withSelfRel());
+		// TODO Auto-generated constructor stub
+	}
+
+	public DressModel link_Update(DressModel dressModel) {
+		// PUT
+		dressModel.add(linkTo(DressController.class).withRel("update-dress"));
+		return dressModel;
+	}
+
+	public DressModel link_Lists(DressModel dressModel) {
+		dressModel.add(linkTo(DressController.class).withRel("lists-dress"));
+		return dressModel;
+	}
+
+}
--- shopping_back/src/main/java/shopping/back/hj/dress/DressService.java
+++ shopping_back/src/main/java/shopping/back/hj/dress/DressService.java
@@ -6,24 +6,30 @@ import java.net.URI;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.ui.ModelMap;
 
 @Service
 public class DressService {
 
 	@Autowired
 	private DressRepository dressRepository;
-	
+
 	@Autowired
 	private ModelMapper modelMapper;
-	
+
 	public ResponseEntity createDress(DressDto dressDto) {
 		Dress dress = modelMapper.map(dressDto, Dress.class);
 		Dress newDress = dressRepository.save(dress);
+		DressModel dressModel = new DressModel(newDress);
 		
-		URI createUri = linkTo(DressController.class).slash(newDress.getId()).toUri();
-		return ResponseEntity.created(createUri).body(newDress);
+		WebMvcLinkBuilder selfLinkBuilder= linkTo(DressController.class).slash(newDress.getId());
+		URI createUri = selfLinkBuilder.toUri();
+		
+		dressModel.link_Lists(dressModel);
+		dressModel.link_Update(dressModel);
+		
+		return ResponseEntity.created(createUri).body(dressModel);
 	}
 }
