8d962ea56ef912044ed8c87cb71b87f33dcb116c
rlaguswhd19 <rlaguswhd19@naver.com>
2020-06-30 12:07:37 +0900

    front test page axios post dressDto test front port change 3000

--- shopping_front/src/components/HelloWorld.vue
+++ shopping_front/src/components/HelloWorld.vue
@@ -1,5 +1,49 @@
 <template>
-	<v-container> </v-container>
+	<v-container>
+		<v-text-field
+			label="Brand"
+			placeholder="브랜드를 입력하세요."
+			outlined
+			v-model="dressDto.brand"
+		></v-text-field>
+		<v-text-field
+			label="article_number"
+			placeholder="품번을 입력하세요."
+			outlined
+			v-model="dressDto.article_number"
+		></v-text-field>
+		<v-text-field
+			label="dress_type"
+			placeholder="옷타입을 입력하세요."
+			outlined
+			v-model="dressDto.dress_type"
+		></v-text-field>
+		<v-text-field
+			label="sex"
+			placeholder="성별을 입력하세요."
+			outlined
+			v-model="dressDto.sex"
+		></v-text-field>
+		<v-text-field
+			label="sale"
+			placeholder="가격을 입력하세요."
+			outlined
+			v-model="dressDto.sale"
+		></v-text-field>
+		<v-text-field
+			label="discount"
+			placeholder="할인율을 입력하세요."
+			outlined
+			v-model="dressDto.discount"
+		></v-text-field>
+		<v-text-field
+			label="explanation"
+			placeholder="설명을 입력하세요."
+			outlined
+			v-model="dressDto.explanation"
+		></v-text-field>
+		<v-btn @click="test_multipart" style="float: right;">등록</v-btn>
+	</v-container>
 </template>
 
 <script>
@@ -10,15 +54,25 @@ export default {
 
 	data() {
 		return {
-			dress: {},
+			dressDto: {
+				brand: '',
+				article_number: '',
+				dress_type: '',
+				sex: '',
+				sale: '',
+				discount: '',
+				explanation: '',
+			},
 		}
 	},
 	methods: {
 		test_multipart: function () {
 			axios({
 				method: 'post',
-				url: 'http://localhost/api/dress',
-				data: this.dress,
+				url: 'http://localhost:8080/api/dress',
+				data: this.dressDto,
+			}).then(response => {
+				console.log(response)
 			})
 		},
 	},
