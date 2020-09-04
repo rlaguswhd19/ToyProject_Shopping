5cffc19f89431b5cd29acbf57539ffd15836b3c4
rlaguswhd19 <rlaguswhd19@naver.com>
2020-09-04 17:38:48 +0900

    back: oauth token corsFilter 구현

--- shopping_back/src/main/java/shopping/back/hj/config/CorsFilterConfig.java
+++ shopping_back/src/main/java/shopping/back/hj/config/CorsFilterConfig.java
@@ -16,25 +16,23 @@ import org.springframework.stereotype.Component;
 
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
-public class FilterConfig implements Filter{
+public class CorsFilterConfig implements Filter{
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		 HttpServletResponse res = (HttpServletResponse) response;
-	        HttpServletRequest req = (HttpServletRequest) request;
-	        res.setHeader("Access-Control-Allow-Origin", "*");
-	        res.setHeader("Access-Control-Allow-Credentials", "true");
-	        res.setHeader("Access-Control-Allow-Methods","*");
-	        res.setHeader("Access-Control-Max-Age", "3600");
-	        res.setHeader("Access-Control-Allow-Headers",
-	            "Origin, X-Requested-With, Content-Type, Accept, Key, Authorization");
-	 
-	        if ("OPTIONS".equalsIgnoreCase(req.getMethod())) {
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse res = (HttpServletResponse) response;
+		res.setHeader("Access-Control-Allow-Origin", "*");
+		res.setHeader("Access-Control-Allow-Credentials", "true");
+		res.setHeader("Access-Control-Allow-Methods","*");
+		res.setHeader("Access-Control-Allow-Headers", "*");
+		
+		 if ("OPTIONS".equalsIgnoreCase(req.getMethod())) {
 	            res.setStatus(HttpServletResponse.SC_OK);
 	        } else {
 	            chain.doFilter(request, response);
-	        }		
+	        }
 	}
 
 }
